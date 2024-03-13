d72c834f073c53d1ab581c4a5ee6739ac9df6d37
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-03-13 09:28:53 -0300

    Envio de html no corpo do e-mail

--- server.js
+++ server.js
@@ -24,15 +24,66 @@ const transporter = nodemailer.createTransport({
 // ROTAS
 app.get('/', (req, res) => res.send('NODE.JS: backend email'))
 
-app.post('/send', async (req, res) => {
+app.get('/send', async (req, res) => {
 
-  const { from, to, subject, text } = req.body
+  const { email, subject, message } = req.body
 
   await transporter.sendMail({
-    from,
-    to,
-    subject,
-    text,
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: subject,
+    html: `<!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Realizado com Sucesso</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #fff;
+        margin: 0;
+        padding: 10px;
+      }
+      .container {
+        max-width: 580px;
+        margin: 20px auto;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+      }
+      h1 {
+        color: #333;
+      }
+      p {
+        color: #666;
+      }
+      .message {
+        background-color: #f9f9f9;
+        padding: 10px;
+        border-radius: 5px;
+        margin-top: 20px;
+      }
+    </style>
+    </head>
+    <body>
+    
+    <div class="container">
+      <h1>Registro Realizado com Sucesso</h1>
+      <p>Prezado(a),</p>
+      <p>Obrigado por se registrar conosco. Seu registro foi realizado com sucesso!</p>
+      <p>Nome: [Nome]</p>
+      <p>Email: ${email}</p>
+      <div class="message">
+        <p>Mensagem deixada pelo usuário:</p>
+        <p>${message}</p>
+      </div>
+      <p>Se tiver alguma dúvida, sinta-se à vontade para nos contatar.</p>
+      <p>Atenciosamente,<br> [Sua Empresa]</p>
+    </div>
+    
+    </body>
+    </html>`
   })
     .then(() => res.status(200).send({message: 'Dados enviados!'}))
     .catch((error) => res.status(400).send({
